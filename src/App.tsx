031b174d6de282eb6eff26b8cdc72c9e2692fec6
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-05-11 16:12:16 +0200

    Define header app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold">Cocktails App</h1>
+    <header className="pl-5 py-5 text-white bg-red-400">
+      <h1 className="text-3xl font-bold">Cocktails App</h1>
+    </header>
   );
 }
 
