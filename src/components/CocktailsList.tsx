ffd842a637a909f929ececd5a0389e4c7c048f3b
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-05-11 19:46:40 +0200

    Delete data fake in CocktailsList

--- src/components/CocktailsList.tsx
+++ src/components/CocktailsList.tsx
@@ -1,26 +1,8 @@
-/* import useCocktails from "../hooks/useCocktails"; */
+import useCocktails from "../hooks/useCocktails";
 import Cocktail from "./Cocktail";
 
 const CocktailsList = () => {
-  /*  const { cocktails } = useCocktails(); */
-
-  const cocktails = [
-    {
-      id: "1",
-      name: "tequila",
-      image: "https://via.placeholder.com/500x200",
-    },
-    {
-      id: "2",
-      name: "tequila2",
-      image: "https://via.placeholder.com/500x200",
-    },
-    {
-      id: "3",
-      name: "tequila3",
-      image: "https://via.placeholder.com/500x200",
-    },
-  ];
+  const { cocktails } = useCocktails();
 
   return (
     <div className="grid grid-cols-3 gap-6">
